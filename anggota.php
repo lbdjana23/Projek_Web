bfe67a5beee99ed0a37589a637fe06ef5486aac2
lbdjana23 <ngurahlabda@gmail.com>
2021-06-16 17:41:47 +0800

    perubahan pada dashboard dan penambahan halaman anggota

--- anggota.php
+++ anggota.php
@@ -1,12 +1,11 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <title>Dashboard</title>
-  <link rel="stylesheet" type="text/css" href="css/dashboard1.css">
-  <script src="https://kit.fontawesome.com/7441b34165.js" crossorigin="anonymous"></script>
+    <title>anggota</title>
+    <link rel="stylesheet" type="text/css" href="css/anggota.css">
 </head>
 <body>
-    <div class="nav">
+<div class="nav">
       <ul>
         <li>KSP</a></i>&nbsp;&nbsp;
         <li><a href="anggota.php"><i class="far fa-user"></i>&nbsp;&nbsp;Anggota</a></i>&nbsp;&nbsp;
@@ -16,17 +15,5 @@
         <li><a href="login.php"><i class="fas fa-sign-out-alt"></i>&nbsp;&nbsp;Logout</a></i>
       </ul>
     </div>
-    <div class="pembatas"></div>
-    <div class="header"></div>
-    <div class="sidebar"></div>
-    <div class="content">
-      <header>
-        <br><div class="bottom">&nbsp;Koperasi Simpan Pinjam</div>
-      </header>
-      <br><hr align="left" width="100%" height="1px" color="white" size="1"><br> &nbsp;Selamat datang di simpan pinjam</h>
-    </div>
-    <div class="footer"></div>
-
-  
 </body>
 </html>
