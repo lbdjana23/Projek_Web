a9461ddd334352b0c7ee253e8dc353f18ba5b459
Febrian90s <febrian90s@example.com>
2021-06-18 15:01:32 +0800

    membuat file sql

---
+++ web_1.sql
@@ -0,0 +1,5 @@
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+INSERT INTO `login` (`id_login`, `username`, `password`, `hak_akses`) VALUES
+('001', 'AnLegian', '09876543', 'Admin'),
