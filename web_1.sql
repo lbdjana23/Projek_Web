b53e4161615d3fa5d2f0538fce5963ffd5b2d760
Febrian90s <febrian90s@example.com>
2021-06-20 12:42:28 +0800

    export ke sql dump

--- web_1.sql
+++ web_1.sql
@@ -1,5 +1,127 @@
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
+/*
+ Navicat Premium Data Transfer
 
-INSERT INTO `login` (`id_login`, `username`, `password`, `hak_akses`) VALUES
-('001', 'AnLegian', '09876543', 'Admin'),
+ Source Server         : koneksi-php
+ Source Server Type    : MySQL
+ Source Server Version : 50141
+ Source Host           : localhost:3306
+ Source Schema         : web_1
+
+ Target Server Type    : MySQL
+ Target Server Version : 50141
+ File Encoding         : 65001
+
+ Date: 20/06/2021 12:41:37
+*/
+
+SET NAMES utf8;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for data_login
+-- ----------------------------
+DROP TABLE IF EXISTS `data_login`;
+CREATE TABLE `data_login`  (
+  `id_login` int(8) NOT NULL AUTO_INCREMENT,
+  `username` varchar(32) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
+  `pass` varchar(32) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
+  `hak_akses` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`id_login`) USING BTREE
+) ENGINE = MyISAM AUTO_INCREMENT = 1 CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of data_login
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for tb_anggota
+-- ----------------------------
+DROP TABLE IF EXISTS `tb_anggota`;
+CREATE TABLE `tb_anggota`  (
+  `id_anggota` int(11) NOT NULL AUTO_INCREMENT,
+  `nama_anggota` varchar(32) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
+  `jenis_kelamin` varchar(16) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
+  `alamat` varchar(60) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
+  `kota` varchar(32) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
+  `telepon` varchar(15) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
+  `email` varchar(50) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
+  `pengurus` int(11) NULL DEFAULT NULL,
+  PRIMARY KEY (`id_anggota`) USING BTREE
+) ENGINE = MyISAM AUTO_INCREMENT = 1 CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of tb_anggota
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for tb_bayar
+-- ----------------------------
+DROP TABLE IF EXISTS `tb_bayar`;
+CREATE TABLE `tb_bayar`  (
+  `id_bayar` int(11) NOT NULL,
+  `id_pinjaman` int(11) NULL DEFAULT NULL,
+  `tangal` date NULL DEFAULT NULL,
+  `angsuran` int(11) NULL DEFAULT NULL,
+  `jumlah_bayar` double NULL DEFAULT NULL,
+  `keterangan` varchar(60) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`id_bayar`) USING BTREE
+) ENGINE = MyISAM CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of tb_bayar
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for tb_jenis
+-- ----------------------------
+DROP TABLE IF EXISTS `tb_jenis`;
+CREATE TABLE `tb_jenis`  (
+  `id_jenis` int(11) NOT NULL,
+  `nama_jenis` int(32) NULL DEFAULT NULL,
+  `keterangan` varchar(60) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`id_jenis`) USING BTREE
+) ENGINE = MyISAM CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of tb_jenis
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for tb_pinjaman
+-- ----------------------------
+DROP TABLE IF EXISTS `tb_pinjaman`;
+CREATE TABLE `tb_pinjaman`  (
+  `id_pinjaman` int(11) NOT NULL,
+  `tanggal` date NULL DEFAULT NULL,
+  `id_anggota` int(11) NULL DEFAULT NULL,
+  `jumlah` double NULL DEFAULT NULL,
+  `bunga` double NULL DEFAULT NULL,
+  `jangka_waktu` int(6) NULL DEFAULT NULL,
+  `keterangan` varchar(50) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
+  `kondisi` varchar(20) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`id_pinjaman`) USING BTREE,
+  INDEX `id_anggota`(`id_anggota`) USING BTREE
+) ENGINE = MyISAM CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of tb_pinjaman
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for tb_simpanan
+-- ----------------------------
+DROP TABLE IF EXISTS `tb_simpanan`;
+CREATE TABLE `tb_simpanan`  (
+  `id_simpanan` int(11) NULL DEFAULT NULL,
+  `tanggal` date NULL DEFAULT NULL,
+  `id_anggota` int(11) NULL DEFAULT NULL,
+  `id_jenis` int(11) NULL DEFAULT NULL,
+  `jumlah` double NULL DEFAULT NULL,
+  `keterangan` varchar(60) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL
+) ENGINE = MyISAM CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of tb_simpanan
+-- ----------------------------
+
+SET FOREIGN_KEY_CHECKS = 1;
